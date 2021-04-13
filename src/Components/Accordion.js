4025eac0634a588aafde0c036dac252cb1e376ea
BilalurRehman <bilal@mavrictech.com>
2021-04-13 23:19:05 +0500

    remove accordion from material core import

--- src/Components/Accordion.js
+++ src/Components/Accordion.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Accordion from '@material-ui/core/Accordion';
-import { AccordionSummary, AccordionDetails } from '@material-ui/core';
+import {
+  AccordionSummary,
+  AccordionDetails,
+  Accordion,
+} from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ListView from '../Components/ListView';
