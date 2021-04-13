f3e30e9e02ba6aa1745c0ceceb9f2a1515ee3e8e
BilalurRehman <bilal@mavrictech.com>
2021-04-13 23:14:02 +0500

    refactor some code

--- src/Components/Accordion.js
+++ src/Components/Accordion.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Accordion from '@material-ui/core/Accordion';
-import AccordionSummary from '@material-ui/core/AccordionSummary';
-import AccordionDetails from '@material-ui/core/AccordionDetails';
+import { AccordionSummary, AccordionDetails } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ListView from '../Components/ListView';
